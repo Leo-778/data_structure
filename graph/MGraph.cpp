38e1c4d538c48a15a9f17d4509b4d17da5067311
1357078925@qq.com <1357078925@qq.com>
2021-05-30 22:40:30 +0800

    master

--- graph/MGraph.cpp
+++ graph/MGraph.cpp
@@ -16,3 +16,58 @@ typedef struct {
     GraphKind kind;                         //记录图的种类
 }MGraph;
 
+void visit(int v){
+
+}
+
+bool visited[MAX_VERtEX_NUM];
+
+void InitQueue(LinkQueue &Q);
+bool IsEmpty(LinkQueue Q);
+bool DeQueue(LinkQueue &Q, BiTree &t);
+void EnQueue(LinkQueue &Q, BiTree t);
+
+void BFS(MGraph G,int V){
+    visit(V);
+    visited[V]=true;
+    EnQueue(Q,V);
+    while(!IsEmpty(Q)){
+        DeQueue(Q, V);
+        for (w=FirstNeighbor(G,V); w>0; w=NextNeighbor(G,V,w)){
+            if (!visited[w]){
+                visit(w);
+                visited[w] = true;
+                EnQueue(Q, V);
+            }
+        }
+    }
+}
+
+void BFSTraverse(MGraph G){
+    for (int i = 0; i < G.vexnum; ++i)
+        visited[i] = false;
+    InitQueue(Q);
+    for (int i = 0; i < G.vexnum; ++i)
+        if(!visited[i])
+            BFS(G, i);
+}
+
+bool visited[MAX_VERTEX_NUM];    //访问标记数组
+void DFS(MGraph G,int V){
+    visit(V);                    //访问初始顶点v
+    visited[V]=true;             //对v作已访问标记
+    while(!IsEmpty(Q)){
+        for (w=FirstNeighbor(G,V); w>0; w=NextNeighbor(G,V,w)){//检测v所有邻接点
+            if (!visited[w]){           //w为v的尚未访问的临界顶点
+                DFS(G,w);
+            }
+        }
+    }
+}
+void DFSTraverse(MGraph G){      
+    for (int i = 0; i < G.vexnum; ++i)
+        visited[i] = false;      //访问标记数组初始化
+    for (int i = 0; i < G.vexnum; ++i)   //从0号顶点开始遍历
+        if(!visited[i])              //对每个连通分量调用依次BFS
+            BFS(G, i);               //vi未访问过，从vi开始BFS
+}
