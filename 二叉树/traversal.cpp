b0794fa9faaca0cf5dd3cf27e4baae7e6c1069ab
1357078925@qq.com <1357078925@qq.com>
2021-07-24 20:29:16 +0800

    master

--- 二叉树/traversal.cpp
+++ 二叉树/traversal.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 const int MaxSize = 100;
 //定义二叉树
 typedef struct BitNode
@@ -144,6 +144,44 @@ Bitree CreateBTree()
     }
     return bt;
 }
+int i, llen, rlen;
+Bitree preincreat(int *a,int *b,int l1,int h1,int l2,int h2){
+    //l1,h1是前序的第一和最后一个节点的下标。l2,h2是中序的第一和最后一个节点的下标。
+    //初始调用时，l1=l2=1,h1=h2=n
+    Bitree root = (BitNode *)malloc(sizeof(BitNode)); 
+    root->key = a[l1];                                
+    for (i = l2; b[i] != root->key ; i++);            //根节点在中序序列的划分
+    llen = i - l2;                                    //左子树长度
+    rlen = h2 - i;                                    //右子树长度
+    if(llen)//递归建立左子树
+        root->lchild = preincreat(a, b, l1 + 1, l1 + llen, l2, l2 + llen - 1);
+    else
+        root->lchild = NULL;
+    if(rlen)//递归建立右子树
+        root->rchild = preincreat(a, b, h1 - rlen + 1, h1, h2 - rlen + 1, h2);
+    else
+        root->rchild = NULL;
+    return root;
+}
+
+Bitree postincreat(int *a,int *b,int l1,int h1,int l2,int h2){
+    //l1,h1是前序的第一和最后一个节点的下标。l2,h2是中序的第一和最后一个节点的下标。
+    //初始调用时，l1=l2=1,h1=h2=n
+    Bitree root = (BitNode *)malloc(sizeof(BitNode)); 
+    root->key = a[h1];                                //根节点
+    for (i = l2; b[i] != root->key ; i++);            //根节点在中序序列的划分
+    llen = i - l2;                                    //左子树长度
+    rlen = h2 - i;                                    //右子树长度
+    if(llen)//递归建立左子树
+        root->lchild = postincreat(a, b, l1 + 1, l1 + llen, l2, l2 + llen - 1);
+    else
+        root->lchild = NULL;
+    if(rlen)//递归建立右子树
+        root->rchild = postincreat(a, b, h1 - rlen + 1, h1, h2 - rlen + 1, h2);
+    else
+        root->rchild = NULL;
+    return root;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -156,3 +194,53 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+
+// 由层序序列和中序序列建立二叉树
+Bitree leveincreat(int n, char *level, char *mid)
+{
+    Bitree root;
+    char left[20];
+    char right[20];
+    int i, j, k;
+    int l, r;
+    int lcnt, rcnt;
+
+    lcnt = 0;
+    rcnt = 0;
+
+    if( n == 0 )
+        return NULL;
+
+    root = (Bitree)malloc(sizeof(BitNode));
+    root->key = level[0];
+
+    // 找到根节点
+    for( i = 0; i < n; i++ )
+        if( level[0] == mid[i] )
+            break;
+
+    // 划分左右子树
+    for( k = 0; k < n; k++ )
+    {
+        for( l = 0; l < i; l++ )
+        {
+            if( mid[l] == level[k] )
+            {
+
+                left[lcnt++] = level[k];
+            }
+        }
+        for( r = 0; r < n-i-1; r++ )
+        {
+            if( mid[r+i+1] == level[k] )
+            {
+                right[rcnt++] = level[k];
+            }
+        }
+    }
+
+    root->lchild = leveincreat( lcnt, left, mid );
+    root->rchild = leveincreat( rcnt, right, mid+i+1 );
+
+    return root;
+}
